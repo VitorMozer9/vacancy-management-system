35a84a63b41f1ab60022098e89574e84bada650d
Vitor Mozer <vectors3whittzz@gmail.com>
2026-05-18 11:21:37 -0300

    feat: add create candidate use case

--- src/main/java/com/github/vitormozer9/management_system/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/github/vitormozer9/management_system/modules/candidate/controllers/CandidateController.java
@@ -1,6 +1,7 @@
 package com.github.vitormozer9.management_system.modules.candidate.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.github.vitormozer9.management_system.exceptions.UserFoundException;
 import com.github.vitormozer9.management_system.modules.candidate.CandidateEntity;
 import com.github.vitormozer9.management_system.modules.candidate.CandidateRepository;
+import com.github.vitormozer9.management_system.modules.candidate.useCases.CreateCandidateUseCase;
 
 import jakarta.validation.Valid;
 
@@ -17,20 +19,18 @@ import jakarta.validation.Valid;
 public class CandidateController {
 
     @Autowired
-    private CandidateRepository candidateRepository;
+    private CreateCandidateUseCase candidateUserCase;
 
     @PostMapping("/")
-    public CandidateEntity createCandidates(@Valid @RequestBody CandidateEntity candidateEntity){
-        this.candidateRepository
-            .findByUsernameOrEmail(candidateEntity.getUsername(),candidateEntity.getEmail())
-            .ifPresent((user) -> {
-                throw new UserFoundException();
-            });
-
-        
-
-
-        return this.candidateRepository.save(candidateEntity);
+    public ResponseEntity<Object> createCandidates(@Valid @RequestBody CandidateEntity candidateEntity){
+        try {
+            var result =  this.candidateUserCase.execute(candidateEntity);
+            return ResponseEntity.ok().body(result);
+        } 
+        catch (Exception e) 
+        {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
 }
---
+++ src/main/java/com/github/vitormozer9/management_system/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -0,0 +1,25 @@
+package com.github.vitormozer9.management_system.modules.candidate.useCases;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.vitormozer9.management_system.exceptions.UserFoundException;
+import com.github.vitormozer9.management_system.modules.candidate.CandidateEntity;
+import com.github.vitormozer9.management_system.modules.candidate.CandidateRepository;
+
+@Service
+public class CreateCandidateUseCase {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    public CandidateEntity execute(CandidateEntity candidateEntity){
+        this.candidateRepository
+            .findByUsernameOrEmail(candidateEntity.getUsername(),candidateEntity.getEmail())
+            .ifPresent((user) -> {
+                throw new UserFoundException();
+            });
+
+        return this.candidateRepository.save(candidateEntity);
+    }
+}
