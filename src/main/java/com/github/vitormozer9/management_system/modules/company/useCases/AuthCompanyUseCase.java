33f75a96b144234383b935852416542dfd640687
Vitor Mozer <vectors3whittzz@gmail.com>
2026-05-20 19:42:40 -0300

    feat: Creating token JWT and add AuthController

--- src/main/java/com/github/vitormozer9/management_system/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/com/github/vitormozer9/management_system/modules/company/useCases/AuthCompanyUseCase.java
@@ -1,16 +1,22 @@
 package com.github.vitormozer9.management_system.modules.company.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.github.vitormozer9.management_system.modules.company.dto.AuthCompanyDTO;
 import com.github.vitormozer9.management_system.modules.company.repositories.CompanyRepository;
 
 @Service
 public class AuthCompanyUseCase {
+
+    @Value("${security.token.secret}")
+    private String secretKey;
 
     @Autowired
     private CompanyRepository companyRepository;
@@ -18,7 +24,7 @@ public class AuthCompanyUseCase {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public void execute(AuthCompanyDTO authCompanyDTO) {
+    public String execute(AuthCompanyDTO authCompanyDTO) {
         var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow(
             () -> {
                 throw new UsernameNotFoundException("Company not found");
@@ -27,11 +33,17 @@ public class AuthCompanyUseCase {
         //Verificar a senha do usuário
         var passwordMatches = this.passwordEncoder.matches(authCompanyDTO.getPassword(), company.getPassword());
 
+        //Se não for igual -> Erro
         if(!passwordMatches) {
-            throw new BadCredentialsException("Invalid username or password");
+            throw new BadCredentialsException("Username/password incorrect");
         }
-            //Se não for igual -> Erro
-            //Se for igual -> Gerar o token
+            
+        //Se for igual -> Gerar o token
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
 
+        var token =JWT.create().withIssuer("javagas")
+            .withSubject(company.getId().toString())
+            .sign(algorithm);
+        return token;
     }
 }
