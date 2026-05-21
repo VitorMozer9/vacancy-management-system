b6e8d1e55b85fd239630797d8f802c3c239f674b
Vitor Mozer <vectors3whittzz@gmail.com>
2026-05-20 21:05:27 -0300

    feat: add SecurityDilter and JWTProvider with validate token

---
+++ src/main/java/com/github/vitormozer9/management_system/providers/JWTprovider.java
@@ -0,0 +1,34 @@
+package com.github.vitormozer9.management_system.providers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+
+@Service
+public class JWTprovider {
+
+    @Value("${security.token.secret}")
+    private String secretKey;
+
+    public String validateToken(String token) {
+        token = token.replace("Bearer ", "");
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+
+        try {
+            var subject = JWT.require(algorithm)
+                             .build()
+                             .verify(token)
+                             .getSubject();   //O ID do nosso usuário
+
+            return subject;
+        } catch (JWTVerificationException ex) {
+            ex.printStackTrace();
+            return "";
+        }
+    }
+    
+}
---
+++ src/main/java/com/github/vitormozer9/management_system/security/SecurityFilter.java
@@ -0,0 +1,51 @@
+package com.github.vitormozer9.management_system.security;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.github.vitormozer9.management_system.providers.JWTprovider;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter{
+
+    @Autowired
+    private JWTprovider jwTprovider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, 
+                                    HttpServletResponse response, 
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+        SecurityContextHolder.getContext().setAuthentication(null);
+        String header = request.getHeader("Authorization");
+
+        if (header != null) {
+            var subjectToken = this.jwTprovider.validateToken(header);
+
+            if(subjectToken.isEmpty()) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+            request.setAttribute("company_id", subjectToken);
+            UsernamePasswordAuthenticationToken auth = 
+            new UsernamePasswordAuthenticationToken(subjectToken, null , Collections.emptyList());
+
+            //Injetando Auth no Spring Security | para todas as requisições o Spring ter as infos do usuário
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+        
+        filterChain.doFilter(request, response);
+    }
+    
+}
