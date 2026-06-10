9ae47bb98196d1bd8baf8f5df9c85bde566f5d30
Vitor Mozer <vectors3whittzz@gmail.com>
2026-06-10 15:45:16 -0300

    feat: create a swagger config class

---
+++ src/main/java/com/github/vitormozer9/management_system/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.github.vitormozer9.management_system.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+
+        return new OpenAPI()
+                .info(new Info().title("Vancancy Management System")
+                        .description("Responsible API for vancancy management").version("1"))
+                .schemaRequirement("jwt_auth", creaSecurityScheme());
+    }
+
+    private SecurityScheme creaSecurityScheme() {
+        return new SecurityScheme().name("jwt_auth").type(SecurityScheme.Type.HTTP).scheme("bearer")
+                .bearerFormat("JWT");
+    }
+
+}
